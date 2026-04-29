9f055d04f2c5c046e018268a6033f7d71c4e8453
Nichlas Ramm <162293775+NRamm99@users.noreply.github.com>
2026-04-29 22:50:42 +0200

    fix navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/NavBar";
 
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Blog from "./pages/Blog";
 import About from "./pages/About";
+import Admin from "./pages/Admin";
 
 import "./App.css";
 
@@ -17,6 +18,9 @@ function App() {
         <Route path="/projects" element={<Projects />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/about" element={<About />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/Admin" element={<Navigate to="/admin" replace />} />
+        <Route path="*" element={<main><h1>404</h1><p>Page not found</p></main>} />
       </Routes>
     </>
   );
